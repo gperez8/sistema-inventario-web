927169020cc025e08f4dc38009ef7148e92ae2bb
Gregory <gregory.facyt@gmail.com>
2014-05-13 15:58:52 -0430

    jquery actualizados

--- Sistema_Inventario/application/views/view_login.php
+++ Sistema_Inventario/application/views/view_login.php
@@ -3,15 +3,14 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- <base href="<?php echo base_url();?>"> Por ahora trabajamos desde localhost -->
 
     <title>Sistema de Inventario</title>
     <link rel="stylesheet" type= "text/css" href = "foundation/css/my_css.css">
     <link rel="stylesheet" href="foundation/css/foundation.css" />
     
 
-    <script src="foundation/js/jquery-1.11.0.min.js"></script>
-    <script src="foundation/js/jquery.js"></script>
+    <script src="foundation/js/jquery-1.11.1.min.js"></script>
+    <script src="foundation/js/jquery-1.11.1.js"></script>
     <script src="foundation/js/foundation.min.js"></script>
     <script src="foundation/js/foundation/foundation.js"></script>  
     <script src="foundation/js/foundation/foundation.orbit.js"></script> 
