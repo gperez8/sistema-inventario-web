274ad8bfa0b27790f680814dcff53c4bb61b2fb7
Gregory <gregory.facyt@gmail.com>
2014-05-13 16:44:02 -0430

    add de jquery-ui

---
+++ Sistema_Inventario/application/views/calendario.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+   
+
+<!DOCTYPE html>
+<html>
+		<head>
+
+			<meta charset="utf-8" />
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+		    <title>Sistema de Inventario</title>
+		    <link rel="stylesheet" type= "text/css" href = "foundation/css/my_css.css">
+		    <link rel="stylesheet" href="foundation/css/foundation.css" />
+		    
+
+		    <script src="foundation/js/jquery-1.11.1.min.js"></script>
+		    <script src="foundation/js/jquery-1.11.1.js"></script>
+
+
+			<title> prueba calendario</title>
+
+		</head>
+	<body>
+
+	</body>
+	
+</html>
--- Sistema_Inventario/application/views/view_login.php
+++ Sistema_Inventario/application/views/view_login.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <title>Sistema de Inventario</title>
-    <link rel="stylesheet" type= "text/css" href = "foundation/css/my_css.css">
-    <link rel="stylesheet" href="foundation/css/foundation.css" />
+    <link rel="stylesheet" type= "text/css" href = "foundation/css/all.css">
+    
     
 
     <script src="foundation/js/jquery-1.11.1.min.js"></script>
